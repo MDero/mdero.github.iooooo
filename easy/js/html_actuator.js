c44d105675469779b9b323ad584d8d2a89690e47
MDero <maxime.derobillard@gmail.com>
2014-04-01 17:02:02 +0200

    Update message win

--- easy/js/html_actuator.js
+++ easy/js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Pas trop dur ? ";
+  var message = won ? "It was very hard..." : "IMPOSSIBRU ! :o";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
