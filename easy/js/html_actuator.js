6b42804bd7bdaea30fa9240745907df5ee7c6a54
MDero <maxime.derobillard@gmail.com>
2014-04-01 17:00:21 +0200

    Finish EASY Mode

--- easy/js/html_actuator.js
+++ easy/js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Ba alors, on est nul ? ";
+  var message = won ? "You win!" : "Pas trop dur ? ";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
